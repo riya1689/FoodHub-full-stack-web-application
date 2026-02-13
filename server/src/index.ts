b20aef215143ba6c87f33c20b0ec91712084ec93
riya1689 <riyaratri24@gmail.com>
2026-02-13 21:26:57 +0600

    Fix: configure serverless deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,28 +1,28 @@
-import express,{Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+
 import authRoutes from './routes/authRoutes';
 import mealRoutes from './routes/mealRoutes';
 import providerRoutes from './routes/providerRoutes';
 import adminRoutes from './routes/adminRoutes';
 import publicRoutes from './routes/publicRoutes';
+
 dotenv.config();
+
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
-app.use('/api/meals',mealRoutes);
+app.use('/api/meals', mealRoutes);
 app.use('/api/provider', providerRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api', publicRoutes);
 
-app.get('/',(req: Request, res: Response) => {
-    res.send('FoodHub API is running...');
+app.get('/', (req: Request, res: Response) => {
+  res.send('FoodHub API is running...');
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`);
-});
+export default app;
