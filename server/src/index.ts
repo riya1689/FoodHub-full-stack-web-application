d4cd3edf361096214bb6a5737cf4d8bc99e53106
riya1689 <riyaratri24@gmail.com>
2026-02-13 18:17:26 +0600

    add provider and admin routes in index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,8 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes';
 import mealRoutes from './routes/mealRoutes';
+import providerRoutes from './routes/providerRoutes';
+import adminRoutes from './routes/adminRoutes';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,6 +14,8 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/meals',mealRoutes);
+app.use('/api/provider', providerRoutes);
+app.use('/api/admin', adminRoutes);
 
 app.get('/',(req: Request, res: Response) => {
     res.send('FoodHub API is running...');
