0e113e7f3f0cec916f9aa402035ee41c5a385179
riya1689 <riyaratri24@gmail.com>
2026-02-15 08:58:34 +0600

    Fix: tailwind and postcss config

---
+++ client/tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        orange: {
+          50: '#fff7ed',
+          100: '#ffedd5',
+          200: '#fed7aa',
+          300: '#fdba74',
+          400: '#fb923c',
+          500: '#f97316',
+          600: '#ea580c', 
+          700: '#c2410c',
+        }
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
