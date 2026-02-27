667e85042755cb3e082d91bc1766ba0e5e0e06b1
riya1689 <riyaratri24@gmail.com>
2026-02-28 02:41:20 +0600

    add menu management route(provider-private)

--- server/src/routes/providerPrivateRoutes.ts
+++ server/src/routes/providerPrivateRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getDashboardStats, getProviderOrders, updateOrderStatus } from '../controllers/providerController';
+import { getDashboardStats, getProviderOrders, updateOrderStatus, addMeal, deleteMeal } from '../controllers/providerController';
 import { authenticateToken } from '../middleware/authMiddleware'; 
 
 const router = express.Router();
@@ -12,5 +12,8 @@ router.get('/orders', authenticateToken, getProviderOrders);
 
 // PATCH /api/provider/orders/:id
 router.patch('/orders/:id', authenticateToken, updateOrderStatus);
+//menu management route
+router.post('/meals', authenticateToken, addMeal);
+router.delete('/meals/:id', authenticateToken, deleteMeal);
 
 export default router;
